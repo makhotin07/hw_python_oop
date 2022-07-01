828d36fa91c8749f86596bcc3f0e58b00a1bbfd7
Никита Махотин <makhotin.07@gmail.com>
2022-07-01 12:22:12 +0300

    текст комментария

--- homework.py
+++ homework.py
@@ -12,10 +12,10 @@ class InfoMessage:
 
     def get_message(self):
         message = (f'Тип тренировки: {self.training_type}; '
-               f'Длительность: {self.duration:.3f} ч.; '
-               f'Дистанция: {self.distance:.3f} км; '
-               f'Ср. скорость: {self.speed:.3f} км/ч; '
-               f'Потрачено ккал: {self.calories:.3f}.')
+                   f'Длительность: {self.duration:.3f} ч.; '
+                   f'Дистанция: {self.distance:.3f} км; '
+                   f'Ср. скорость: {self.speed:.3f} км/ч; '
+                   f'Потрачено ккал: {self.calories:.3f}.')
         
         return message
 
